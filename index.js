581311ed683a281ebe9f567b5afdefab25db339f
TTC Dev <ttcdev@TTC-Dev.local>
2017-01-09 17:56:16 -0500

    can now add things to database

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
+var bodyParser =require('body-parser');
 
 
 mongoose.connect('mongodb://localhost/db');
@@ -9,19 +10,18 @@ mongoose.connect('mongodb://localhost/db');
 
 
 var blogModel = mongoose.model('Blog', {
-    blogName: String,
-    entries: [{
-        id: Number,
-        title: String,
-        date: Date,
-        content: String,
-        image: String
-    }]
+    id: Number,
+    title: String,
+    date: Date,
+    content: String,
+    image: String
 });
 
 
 app.listen(8080);
 console.log("App listening on port 8080");
+
+app.use(bodyParser());
 
 app.get('/', function(req, res) {
     //res.send('Welcome to my world... (BLOG)');
@@ -30,7 +30,19 @@ app.get('/', function(req, res) {
         if (err) {
             res.send(err);
         }else {
-            res.json(entries);
+            res.send(entries);
         }
     });
-})
+});
+
+app.post('/', function(req, res, next) {
+    blogModel.create({
+        title : req.body.title,
+        // add more stuff later
+        },
+        function (err, entry) {
+            if (err) res.send(err);
+        }
+    );
+    }
+);
