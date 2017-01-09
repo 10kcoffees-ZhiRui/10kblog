41e665e01bab8ae20e6409f4a30711c86d8f3e2c
TTC Dev <ttcdev@TTC-Dev.local>
2017-01-09 15:20:34 -0500

    added some routing

--- index.js
+++ index.js
@@ -2,8 +2,35 @@ var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
 
+
 mongoose.connect('mongodb://localhost/db');
+
+//var db = mongoose.connection;
+
+
+var blogModel = mongoose.model('Blog', {
+    blogName: String,
+    entries: [{
+        id: Number,
+        title: String,
+        date: Date,
+        content: String,
+        image: String
+    }]
+});
 
 
 app.listen(8080);
 console.log("App listening on port 8080");
+
+app.get('/', function(req, res) {
+    //res.send('Welcome to my world... (BLOG)');
+
+    blogModel.find(function(err, entries) {
+        if (err) {
+            res.send(err);
+        }else {
+            res.json(entries);
+        }
+    });
+})
