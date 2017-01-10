5497b50aa6415a7e91e2e87bc8f72c66f5686ecd
TTC Dev <ttcdev@TTC-Dev.local>
2017-01-10 11:30:54 -0500

    started on the frontend

---
+++ app.js
@@ -0,0 +1,3 @@
+/**
+ * Created by ttcdev on 2017-01-10.
+ */
--- index.js
+++ index.js
@@ -12,7 +12,7 @@ mongoose.connect('mongodb://localhost/db');
 var blogModel = mongoose.model('Blog', {
     id: Number,
     title: String,
-    date: Date,
+    date: {type: Date, default: Date.now },
     content: String,
     image: String
 });
@@ -21,27 +21,29 @@ var blogModel = mongoose.model('Blog', {
 app.listen(8080);
 console.log("App listening on port 8080");
 
-app.use(bodyParser());
+app.get('*', function(req, res) {
+    res.sendFile('index.html', { root: '/Users/ttcdev/Documents/blog'});
+});
 
-app.get('/', function(req, res) {
+app.use(bodyParser.urlencoded({ extended: true}));
+
+app.get('/api', function(req, res) {
     //res.send('Welcome to my world... (BLOG)');
 
     blogModel.find(function(err, entries) {
-        if (err) {
-            res.send(err);
-        }else {
-            res.send(entries);
-        }
+        if (err) res.send(err);
+        res.send(entries);
     });
 });
 
-app.post('/', function(req, res, next) {
+app.post('/api', function(req, res, next) {
     blogModel.create({
         title : req.body.title,
         // add more stuff later
         },
         function (err, entry) {
             if (err) res.send(err);
+            res.end();
         }
     );
     }
